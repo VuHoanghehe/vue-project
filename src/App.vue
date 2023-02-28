8e5f5fce1ea9ef5d7ecdcf1cd42fb6a37b99955f
Vu Hoang <vu30887@gmail.com>
2023-02-28 19:17:04 +0700

    new

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <myInput />
+    <myButton background="darkslateblue" color="white" :disabled="!valid" />
+</template>
+
+<script>
+import myButton from "./components/myButton.vue";
+import myInput from "./components/myInput.vue"
+
+export default {
+    components: {
+        myButton,
+        myInput,
+    },
+    data() {
+        return {
+            valid: true,
+        };
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/myInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="label">
+    <label>Username</label>
+  </div>
+  <input name="username">
+</template>
+
+<script>
+export default {
+  props: {
+   
+  },
+};
+</script>
+
+<style scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+}
+.label {
+  display: flex;
+  justify-content: space-between;
+}
+.error {
+  color: red;
+}
+.input {
+  width: 100%;
+}
+input {
+  box-sizing: border-box;
+  padding: 10px;
+  border-radius: 8px;
+  border: 1px solid silver;
+  margin: 2px;
+  font-size: 16px;
+  width: 100%;
+  cursor: pointer;
+}
+</style>
